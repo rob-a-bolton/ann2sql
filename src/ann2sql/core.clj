e60ced3e1f803ffd7bb33012d0dbbff146d73be2
rob.a.bolton <rob.a.bolton@gmail.com>
2021-09-24 12:32:33 +0100

    Version 1.0.0, working postgresql import

--- src/ann2sql/core.clj
+++ src/ann2sql/core.clj
@@ -16,23 +16,30 @@
       .toLowerCase
       keyword))
 
-;; (s/def :rec/record)
+(def cols 
+  [[:hadm-id :int]
+   [:row-id :int]
+   [:id :int]
+   [:tui :text]
+   [:cui :text]
+   [:source-value :text]
+   [:status :text :null]
+   [:m-start :int]
+   [:m-end :int]
+   [:acc :float]])
 
 (defn ann->map
   [{:keys [id tui cui start end acc source-value meta-anns]}]
-  {:id id :tui tui :cui cui :source-value source-value
-   :start (Integer/valueOf start)
-   :end (Integer/valueOf end)
-   :acc (Double/valueOf acc)
-   :status (get-in meta-anns [:status :value])})
+  [(Integer/valueOf id) tui cui source-value
+   (Integer/valueOf start)
+   (Integer/valueOf end)
+   (Double/valueOf acc)
+   (get-in meta-anns [:status :value])])
 
 (defn record->map
   [record]
-  ;; {:pre [s/valid? :rec/record record]}
   (let [{:keys [hadm-id row-id annotations]} record]
-    ;; (doall (map #(println (format "[%s] %s: %s" hadm-id row-id (:cui %)))
-    ;;             annotations))
-    (map #(merge {:hadm-id hadm-id :row-id row-id}
+    (map #(concat [(Integer/valueOf hadm-id) (Integer/valueOf row-id)]
                  (ann->map %))
          annotations)))
 
@@ -41,53 +48,44 @@
   (with-open [i (io/reader file)]
     (record->map (cheshire/parse-stream i json-k->kw))))
 
-(defn get-record-stmts
+(defn get-record-rows
   [dir]
   (->> (file-seq (io/file dir))
        (filter #(.isFile %))
        (map file->record-map)
-       flatten
-       (partition-all 1000)
-       (map (fn [anns]
-              (sql/format {:insert-into :annotations :values anns}
-                          {:pretty true})))))
-
-(defn prepare-bulk-stmt
-  [table data]
-  (sql/format 
-   {:insert-into table
-    :values (list (first data))}))
+       (apply concat)
+       (partition-all 1000)))
 
 (defn import-records
   [{:keys [dir jdbc]}]
-  (with-open [db-con (jdbc/get-connection {:jdbcUrl jdbc})]
+  (with-open [db-con (jdbc/get-connection {:jdbcUrl jdbc} {:auto-commit false})]
     (reset! con db-con)
     (jdbc/execute!
      db-con
      (sql/format {:create-table [:annotations :if-not-exists]
-                  :with-columns
-                  [[:hadm-id :int]
-                   [:row-id :int]
-                   [:id :int]
-                   [:tui :text]
-                   [:cui :text]
-                   [:source-value :text]
-                   [:status :text :null]
-                   [:start :int]
-                   [:end :int]
-                   [:acc :float]]}))
-    (let [stmts (get-record-stmts dir)]
+                  :with-columns cols}))
+    (.commit db-con)
+    (let [stmts (get-record-rows dir)]
       (doseq [batch stmts]
-        (jdbc/execute! db-con batch)))))
+        (with-open [p (jdbc/prepare db-con ["INSERT INTO annotations (hadm_id, row_id, id, tui, cui, source_value, m_start, m_end, acc, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"])]
+          (doseq [row batch]
+            (.setInt p 1 (nth row 0)) ;hadm
+            (.setInt p 2 (nth row 1)) ;row
+            (.setInt p 3 (nth row 2)) ;id
+            (.setString p 4 (nth row 3)) ;tui
+            (.setString p 5 (nth row 4)) ;cui
+            (.setString p 6 (nth row 5)) ;source
+            (.setInt p 7 (nth row 6)) ;m_start
+            (.setInt p 8 (nth row 7)) ;m_end
+            (.setFloat p 9 (nth row 8)) ;acc
+            (.setString p 10 (nth row 9)) ;status
+            (.executeUpdate p)))
+        (.commit db-con)))))
 
 (def cli-configuration
   {:command "ann2sql"
    :description "Import annotation JSON records into a database"
-   :version "0.0.1"
-  ;;  :opts [{:option "dir"
-  ;;          :default "."
-  ;;          :type :string
-  ;;          :as "The dir to search for JSON records"}]
+   :version "1.0.0"
    :opts [{:option "jdbc"
            :default :present
            :type :string
@@ -102,5 +100,4 @@
 
 (defn -main
   [& args]
-  (run-cmd args cli-configuration)
-  (println "f"))
+  (run-cmd args cli-configuration))
